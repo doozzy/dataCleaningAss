b929670ec9da4c40224e4280534f3891d397eb9a
doozzy <oldoozd@gmail.com>
2016-02-05 21:16:22 +1100

    Final version

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,9 @@
 library(dplyr)
+library(reshape2)
 
 testdata<-read.table("UCI HAR Dataset/test/X_test.txt")
 traindata<-read.table("UCI HAR Dataset/train/X_train.txt")
-#merge tain and test data sets
+#1. merge tain and test data sets
 wholedata <-rbind(traindata,testdata)
 
 testsubject<-read.table("UCI HAR Dataset/test/subject_test.txt")
@@ -18,27 +19,31 @@ activity <-rbind(trainactivity ,testactivity)
 wholedata$activity<-activity[[1]]
 
 features <- read.table("UCI HAR Dataset/features.txt")
+#4. descriptive names for variables
+features[[2]]<-sub("t","time",features[[2]])
+features[[2]]<-sub("f","frequency",features[[2]])
+features[[2]]<-sub("\\(\\)","",features[[2]])
 
 names(wholedata) <- features[[2]]
 
 colnames(wholedata)[562] <- "subject"
 colnames(wholedata)[563] <- "Activity"
 
-#Extracts only the measurements on the mean and standard deviation for each measurement.
+#2. Extracts only the measurements on the mean and standard deviation for each measurement.
 meanstd<-wholedata[,grep("mean|std", names(wholedata))]
 
 
-#Uses descriptive activity names to name the activities in the data set
+#3. Uses descriptive activity names to name the activities in the data set
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 activity<-ordered(activity[1:10299,],levels=activity_labels$V1,labels=activity_labels$V2)
 
-wholedata$activity<-activity
+wholedata$activity<-activity 
 
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for 
-each activity and each subject.
+#5. creates a second, independent tidy data set with the average of each variable for 
+#each activity and each subject.
 library(reshape2)
 mdata <- melt(wholedata, id=c("activity","subject"))
-subjmean <- dcast(mdata, subject~variable, mean)
-actimean <- dcast(mdata, activity~variable, mean)
+actiSubmean <- dcast(mdata, subject + activity~variable, mean)
+write.table(actiSubmean,file ="tidydataset.txt", row.name=FALSE)
